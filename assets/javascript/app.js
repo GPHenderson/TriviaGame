5cc6156f02d217f8f6055cb247c782b6620fddde
Gregory Henderson <gh_03@yahoo.com>
2019-08-24 23:40:19 -0500

    Updating the CSS, Images, JS, and HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,17 @@ var questions = [{
     answers: ["A Bug's Life", "Monster Inc", "Toy Story", "The Lion King"], 
     correctAnswer: "Toy Story"
 }];
+var questions = [{
+    question: "Which of theses is NOT a name of on of the Spice Girls?",
+    answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"], 
+    correctAnswer: "Fred Spice"
+}];
+
+var questions = [{
+    question: "Which NBA team won the most titles in the 90's?",
+    answers: ["Bulls", "Lakers", "Celtics", "Knicks"], 
+    correctAnswer: "Bulls"
+}];
 
 var game = {
     correct: 0,
@@ -18,14 +29,15 @@ var game = {
     countdown: function(){
         game.counter--;
         $('#counter').html(game.counter);
-        if (game.counter<=0){
+        if(game.counter<=0){
             console.log ("Time is up!");
-            game.done ();
+            game.done();
 
         }
+    
     },
 start: function(){
-    timer= setInterval (game.countdown, 1000);
+    timer = setInterval(game.countdown, 1000);
     $('#subwrapper').prepend('<h2>Time Remaining: <span id = "counter">20</span> Seconds </h2>');
     $('#start').remove ();
     for (var i=0; i<questions.length; i++){
@@ -43,7 +55,7 @@ start: function(){
           }else {
               game.incorrect++;
           }
-      })
+      });
       this.result ();
 }, 
 result: function (){
@@ -57,4 +69,4 @@ result: function (){
 
 
 }
-}
+};
