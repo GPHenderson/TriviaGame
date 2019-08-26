afc9f0e3f18db0877c9a1f920c2de80558d9b682
Gregory Henderson <gh_03@yahoo.com>
2019-08-25 22:20:06 -0500

    updating the JS to correct the error with the counter and DOM update error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,78 @@
 $('#start').on ('click', function(){
     game.start ();
 })
+
 $(document).on('click', '#end',function(){
     game.done();
 })
 
-var questions = [{
-    question: "What was the first full-length CGI movie?",
-    answers: ["A Bug's Life", "Monster Inc", "Toy Story", "The Lion King"], 
-    correctAnswer: "Toy Story"
-}];
+
 var questions = [{
     question: "Which of theses is NOT a name of on of the Spice Girls?",
     answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"], 
     correctAnswer: "Fred Spice"
-}];
+},
+    {
+        question: "Which NBA team won the most titles in the 90's?",
+        answers: ["Bulls", "Lakers", "Celtics", "Knicks"],
+        correctAnswer: "Bulls"
+    },
 
-var questions = [{
-    question: "Which NBA team won the most titles in the 90's?",
-    answers: ["Bulls", "Lakers", "Celtics", "Knicks"], 
-    correctAnswer: "Bulls"
-}];
+    {
+        question: "What was the first full-length CGI movie?",
+        answers: ["A Bug's Life", "Monster Inc", "Toy Story", "The Lion King"],
+        correctAnswer: "Toy Story"
+    }
+];
+
+
 
 var game = {
     correct: 0,
     incorrect: 0, 
     counter: 20,
+    questionCount: 0,
+    numberQuestions: 3,
     countdown: function(){
         game.counter--;
-        $('#counter').html(game.counter);
-        if(game.counter<=0){
+        if(game.counter<0){
             console.log ("Time is up!");
             game.done();
-
         }
-    
+        $('#counter').html(game.counter);
     },
 start: function(){
     timer = setInterval(game.countdown, 1000);
-    $('#subwrapper').prepend('<h2>Time Remaining: <span id = "counter">20</span> Seconds </h2>');
-    $('#start').remove ();
-    for (var i=0; i<questions.length; i++){
-        $('#subwrapper').append('<h2>'+ questions [i].question+ '</h2>');
-        for (var j=0; j<questions[i].answers.length;j++){
-            $("#subwrapper").append ("<input type= 'radio' name='question-"+i+"' value= '"+questions[i].answers[j]+"'>"+questions[i].answers[j])
+
+
+        $('#subwrapper').prepend('<h2 id="clock">Time Remaining: <span id = "counter">20</span> Seconds </h2>');
+        $('#start').remove();
+
+        for(var count = 0; count < questions.length; count++) {
+
+            $('#question').append('<h2>' + questions [count].question + '</h2>');
+            for (var j = 0; j < questions[count].answers.length; j++) {
+                $('#question').append("<input type= 'radio' name='answer"+count+"' value= '" + questions[count].answers[j] + "'>" +
+                    "" + questions[count].answers[j])
+            }
+
         }
-    }
-    $('#subwrapper').append('<br><button id="end">Done</button>');
+        $('#question').append('<br><br><button id="end">Done</button>');
   }, 
   done: function (){
-      $.each ($('input[name="question-0]" :checked'), function (){
-          if ($(this).val ()==questions [0].correctAnswer){
-              game.correct++;
-          }else {
-              game.incorrect++;
-          }
-      });
+        for(var doneCount=0; doneCount< questions.length; doneCount++) {
+            var answer = $("input[name='answer"+doneCount+"']:checked").val();
+            if(answer == questions[doneCount].correctAnswer){
+                game.correct++;
+            } else{
+                game.incorrect++;
+            }
+      }
       this.result ();
 }, 
 result: function (){
     clearInterval(timer);
+
     $('#subwrapper h2').remove();
 
     $('#subwrapper').html("<h2>All done!</h2>");
